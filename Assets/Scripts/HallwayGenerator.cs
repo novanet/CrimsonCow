7e5c4d1f05a6be223277a80e1b6b76320dc9dd8d
Thomas <thomas.karoliussen@novanet.no>
2019-05-05 22:02:16 +0200

    Two new obstacles

--- Assets/Scripts/HallwayGenerator.cs
+++ Assets/Scripts/HallwayGenerator.cs
@@ -11,37 +11,48 @@ public class HallwayGenerator : MonoBehaviour
     {
         for (var i = 0; i < NumberOfSegments; i++)
         {
-            // ceiling
-            CreateWall(
-                new Vector3(Width, 1, Depth),
-                new Vector3(0, Height / 2 + WallThickness / 2, Depth * i));
-            
-            // floor
-            CreateWall(
-                new Vector3(Width, 1, Depth),
-                new Vector3(0, - Height / 2 - WallThickness / 2, Depth * i));
-
-            // right wall
-            CreateWall(
-                new Vector3(1, Height, Depth),
-                new Vector3(Width / 2 + WallThickness / 2, 0, Depth * i));
-
-            // left wall
-            CreateWall(
-                new Vector3(1, Height, Depth),
-                new Vector3(- Width / 2 - WallThickness / 2, 0, Depth * i));
+            CreateEmptySegment($"Segment{i}", new Vector3(0, 0, Depth * i));
         }
     }
 
-    private void CreateWall(Vector3 size, Vector3 position)
+    private void CreateEmptySegment(string name, Vector3 position)
     {
-        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        cube.transform.localScale = size;
-        cube.transform.position = position;
+        // Create empty gameObject per segment
+        var segment = new GameObject(name).transform;
+        segment.parent = transform;
+            
+        // ceiling
+        CreateWall(
+            new Vector3(Width, 1, Depth),
+            new Vector3(0, Height / 2 + WallThickness / 2, 0) + position,
+            segment);
+            
+        // floor
+        CreateWall(
+            new Vector3(Width, 1, Depth),
+            new Vector3(0, - Height / 2 - WallThickness / 2, 0) + position,
+            segment);
+
+        // right wall
+        CreateWall(
+            new Vector3(1, Height, Depth),
+            new Vector3(Width / 2 + WallThickness / 2, 0, 0) + position,
+            segment);
+
+        // left wall
+        CreateWall(
+            new Vector3(1, Height, Depth),
+            new Vector3(- Width / 2 - WallThickness / 2, 0, 0) + position,
+            segment);
     }
 
-    void Update()
+    private void CreateWall(Vector3 size, Vector3 position, Transform parent)
     {
+        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube).transform;
+        cube.gameObject.name = "Wall";
         
+        cube.localScale = size;
+        cube.position = position;
+        cube.parent = parent;
     }
 }
