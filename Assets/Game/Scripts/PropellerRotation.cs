9c3af05ff78cc578b160a2ea5bb54a2f0e025f9c
Lars Jakobsen <larsajakobsen@gmail.com>
2019-05-20 12:00:03 +0200

    Added menu and a obstacle

---
+++ Assets/Game/Scripts/PropellerRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerRotation : MonoBehaviour
+{
+    public float RotationsPerSecond = 5f;
+    
+    void Update()
+    {
+        var angle = 360 * (RotationsPerSecond * Time.deltaTime);
+        transform.Rotate(Vector3.forward, angle, Space.Self);
+    }
+}
