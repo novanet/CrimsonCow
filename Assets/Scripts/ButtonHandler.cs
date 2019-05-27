d63c28e5ca2f9d6b90e70f3f91365af1e1524ef3
Lars Jakobsen <larsajakobsen@gmail.com>
2019-05-27 12:45:35 +0200

    Add menu

---
+++ Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void OnNewGameClicked()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnQuitClicked()
+    {
+        Application.Quit();
+    }
+}
