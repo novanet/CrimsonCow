48cfd808602353bda08d3c5db6107d3bfef10e52
Lars Jakobsen <larsajakobsen@gmail.com>
2019-06-05 10:27:49 +0200

    Add new obstacle, double fan

---
+++ Assets/Scripts/RotateXNegative.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateXNegative : MonoBehaviour
+{
+    [Tooltip("Degrees per second")]
+    public float Speed = 60;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Speed * Time.deltaTime * -1, 0, 0);
+    }
+}
