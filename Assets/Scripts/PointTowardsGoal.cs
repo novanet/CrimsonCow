403ce4c4d867008bbf1abb80d5c9d6961b55a215
Thomas <thomas.karoliussen@novanet.no>
2019-06-18 22:29:50 +0200

    Better arrow rotation, better lighting of spotlights

--- Assets/Scripts/PointTowardsGoal.cs
+++ Assets/Scripts/PointTowardsGoal.cs
@@ -4,17 +4,8 @@ using UnityEngine;
 
 public class PointTowardsGoal : MonoBehaviour
 {
-    private Vector3 _goal;
-    
-    public Camera Camera;
-
-    public void Start()
-    {
-        _goal = new Vector3(0, 0, float.MaxValue);
-    }
-
     public void Update()
     {
-        transform.localRotation = Quaternion.Euler(-Camera.transform.rotation.eulerAngles);
+        transform.rotation = Quaternion.identity;
     }
 }
