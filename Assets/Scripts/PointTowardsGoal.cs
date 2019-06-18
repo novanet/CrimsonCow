baba01a767668c248e4b3749166ce5f2cce06e98
Thomas <thomas.karoliussen@novanet.no>
2019-06-18 21:52:49 +0200

    Made an arrow prefab

---
+++ Assets/Scripts/PointTowardsGoal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointTowardsGoal : MonoBehaviour
+{
+    private Vector3 _goal;
+
+    private void Start()
+    {
+        _goal = new Vector3(0, 0, float.MaxValue);
+    }
+
+    void Update()
+    {
+        transform.LookAt(_goal);
+    }
+}
